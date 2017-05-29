b415aa1a0c09f8accf161c83115022d960bf253a
Jon <jon.bent@me.com>
2017-05-29 12:52:13 -0700

    removes a migration that breaks everything

--- db/migrate/20170528201117_change_paydate_to_date_in_payday.rb
+++
@@ -1,5 +0,0 @@
-class ChangePaydateToDateInPayday < ActiveRecord::Migration[5.0]
-  def change
-    change_column :paydays, :paydate, :date
-  end
-end
--- db/migrate/20170528202947_change_paydate_to_date.rb
+++
@@ -1,5 +0,0 @@
-class ChangePaydateToDate < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :paydays, :paydate
-  end
-end
